f27e5a62797c2ef4789bfda6ccbe9548fd29a63b
adrianobrito <adrianosdebrito@gmail.com>
2019-12-12 22:49:52 +0100

    [infra] define compiler version

--- src/main/java/com/github/adrianobrito/ExceptionHandler.java
+++ src/main/java/com/github/adrianobrito/ExceptionHandler.java
@@ -1,10 +1,5 @@
 package com.github.adrianobrito;
 
-import net.jodah.typetools.TypeResolver;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
 /**
  * Created by adriano on 13/11/19.
  */
